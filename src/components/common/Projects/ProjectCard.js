abde3e5de8eaea532426a51bda62887cff3b2eb4
dbbrowne <duncan.b.browne@gmail.com>
2022-02-26 02:35:22 +0000

    feat(projects): tags

--- src/components/common/Projects/ProjectCard.js
+++ src/components/common/Projects/ProjectCard.js
@@ -1,6 +1,6 @@
 import { ReactComponent as DocsIcon } from '../../../assets/docsIcon.svg'
 import { ReactComponent as GithubIcon } from '../../../assets/githubIcon.svg'
-import { ReactComponent as LinkOuticon } from '../../../assets/link-out-svgrepo-com.svg'
+import { ReactComponent as LinkOutIcon } from '../../../assets/link-out-svgrepo-com.svg'
 
 export default function ProjectCard({ project }) {
   return (
@@ -15,18 +15,33 @@ export default function ProjectCard({ project }) {
       </div>
       <h3>{project.title}</h3>
       <p dangerouslySetInnerHTML={{ __html: project.summary }}></p>
-      
 
+      <div className='tags-container'>
+        {
+          project.tags.map(tag=>{
+            return (<span className="tag" key={tag}>{tag}</span>)
+          })
+        }
+      </div>
         
       <ul>
-        <a href={project.links.github}>
+        <a 
+          href={project.links.github}
+          aria-label={`${project.title} Github`}
+        >
           <GithubIcon />
         </a>
-        <a href={project.links.caseStudy}>
+        <a 
+          href={project.links.caseStudy || project.links.github}
+          aria-label={`${project.title} case study`}
+        >
           <DocsIcon />
         </a>
-        <a href={project.links.deploy}>
-          <LinkOuticon />
+        <a 
+          href={project.links.deploy || project.links.github}
+          aria-label={`${project.title} Live Site`}
+        >
+          <LinkOutIcon />
         </a>
       </ul>
     </article>
