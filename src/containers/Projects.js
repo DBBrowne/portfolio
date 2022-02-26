c157f5c903e2bc77fa8c4c086fac888fd8506607
dbbrowne <duncan.b.browne@gmail.com>
2022-02-26 18:51:46 +0000

    style(technologies): basic lists

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -3,7 +3,7 @@ import { projects } from '../content'
 
 export default function Projects () {
   return (
-    <div className='projects container'>
+    <div className='projects-container'>
       {
         projects.map(project => {
           return (
