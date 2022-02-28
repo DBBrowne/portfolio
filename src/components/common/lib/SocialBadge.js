289b1d7540497c0887849a5bd7239b50540c3ee4
dbbrowne <duncan.b.browne@gmail.com>
2022-02-28 09:33:32 +0000

    fix(components): delete duplicate files

--- src/components/common/lib/SocialBadge.js
+++
@@ -1,8 +0,0 @@
-export default function SocialBadge ({ logoUrl, logoAltText, badgeText, badgeLinkUrl }){
-  return (
-    <a className="social-badge" href={badgeLinkUrl}>
-      <img src={logoUrl} alt={logoAltText} />
-      <span>{badgeText}</span>
-    </a>
-  )
-}
